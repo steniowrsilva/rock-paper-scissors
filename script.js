c4939499e76e3874bd754af446bcb7a908a05087
steniowrsilva <steniowoney@gmail.com>
2023-02-09 20:17:05 -0300

    create fucntions getComputerChoice and playRound

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice () {
+    // it'll return Rock, Paper or Scissors
+
+}
+
+function playRound(playerSelection, computerSelection){
+    // returns a string that declares the winner of
+    // the round like so: "You Lose! paper beats Rock".
+
+    // Make your function’s playerSelection parameter case-insensitive
+    // (so users can input rock, ROCK, RocK or any other variation).
+    
+}
+
