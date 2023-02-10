d6b97b6284c4fc38771e89605146c0ec5e101b37
steniowrsilva <steniowoney@gmail.com>
2023-02-09 21:25:21 -0300

    Implement getComputerChoice and playRound

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 function getComputerChoice () {
     // it'll return Rock, Paper or Scissors
-
+    let randomNumber = Math.floor(Math.random()*9);
+    if (randomNumber < 3) {
+        return "rock";
+    } else if ( randomNumber > 3 && randomNumber < 9) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function playRound(playerSelection, computerSelection){
     // returns a string that declares the winner of
     // the round like so: "You Lose! paper beats Rock".
-
-    // Make your function’s playerSelection parameter case-insensitive
-    // (so users can input rock, ROCK, RocK or any other variation).
+    let player = playerSelection.toLowerCase();
+    if (player === "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return "Tie!";
+            case "paper":
+                return "You lose!";
+            case "scissors":
+                return "You win!";
+        }
+    } else if (player === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                return "You win!";
+            case "paper":
+                return "Tie!";
+            case "scissors":
+                return "You lose!";
+        }
+    } else {
+        switch (computerSelection) {
+            case "rock":
+                return "You lose!";
+            case "paper":
+                return "You win!";
+            case "scissors":
+                return "Tie!";
+        }
+    }
     
 }
 
+function game() {
+    for (let i=0; i < 5; i++) {
+        
+    }
+}
